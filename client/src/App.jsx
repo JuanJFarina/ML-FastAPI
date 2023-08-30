fe18114085bb783e0e69cf66af60a14470d0a615
Juan José Farina <juanjosefarina.jjf@gmail.com>
2023-08-30 01:45:33 -0300

    added client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,21 @@
-import { useRef, useEffect, useState } from 'react';
+import { useRef, useState } from 'react';
 import './App.css';
 import axios from 'axios';
 
 function App() {
   const textRef = useRef(null);
   const [translation, setTranslation] = useState('');
+  const [answer, setAnswer] = useState('');
 
   const getTranslation = () => {
+    setTranslation('');
     axios
       .post('https://ml-fast-api.vercel.app/translate?api_key=key123', {
         texto: textRef.current.value
       })
       .then(response => {
         if(response.data.idioma_original !== 'es') {
-          setTranslation(response.data.traduccion);
+          setTranslation(`You said "${response.data.traduccion}" in spanish`);
         }
       })
       .catch(error => {
@@ -21,13 +23,41 @@ function App() {
       });
   };
 
+  const getSentiment = () => {
+    setAnswer('');
+    axios
+      .post('https://ml-fast-api.vercel.app/analyze?api_key=key123', {
+        texto: textRef.current.value
+      })
+      .then(response => {
+        if(response.data.sentimiento === 'positivo') {
+          setAnswer('Nos alegramos muchísimo que hayas tenido una grata experiencia !');
+        }
+        else if(response.data.sentimiento === 'negativo') {
+          setAnswer('Lo lamentamos mucho ! Esperamos nos des otra oportunidad :(');
+        }
+        else {
+          setAnswer('Muchas gracias por tu respuesta !');
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
+
+  const handleSubmit = () => {
+    getTranslation();
+    getSentiment();
+  }
+
   return (
     <main>
       <h2>Hola ! Nos encantaría que nos dejes un comentario sobre nuestro servicio :)</h2>
-      <h6>PS: if you don't know Spanish, please write in English or any other language, and we will translate it for you !</h6>
+      <h6>PS: please leave us a message, it doesn't matter if you don't know Spanish, we will translate it for you !</h6>
       <textarea ref={textRef} /><br />
-      <input type="button" value="Enviar" onClick={getTranslation} />
+      <input type="button" value="Enviar" onClick={handleSubmit} />
       <p>{translation}</p>
+      <h3>{answer}</h3>
     </main>
   );
 }
